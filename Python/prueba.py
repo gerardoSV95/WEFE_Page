37f923ee89c51d6761fd5133121a264c14f34126
gerardoSV95 <69478582+gerardoSV95@users.noreply.github.com>
2021-07-16 17:44:54 -0500

    lol

---
+++ Python/prueba.py
@@ -0,0 +1,35 @@
+#Ejercicio 1
+
+""" a= float(input("Ingrese el valor de a: "))
+b= float(input("Ingrese el valor de b: "))
+c= float(input("Ingrese el valor de c: "))
+
+r=((c+5)*(b**2-(3*a*c)))*(a**2)/(4*a)
+print("El resultado de la ecuacion es: ",r) """
+
+#Ejercicio 2 
+""" a= float(input("Ingrese el valor de a: "))
+b= float(input("Ingrese el valor de b: "))
+c=0
+"" c=a
+a=b
+b=c ""
+a,b=b,a
+print("a=",a)
+print("b=",b) """
+
+#Ejercicio 3
+""" import math
+radio= float(input('Ingrese el radio: '))
+
+area = math.pi*radio**2
+longitud = 2*math.pi*radio
+
+print(f"El area del circulo es: {area}")
+print(f"La longitud del circulo es: {longitud}")
+ """
+#Ejercicio 4
+precio = float(input("Ingrese el precio: "))
+p=0.36*precio
+print(f"El precio final con el 30 de descuento es: {precio-p}")
+
